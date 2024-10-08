e3f31a230c4a60919d3ae94066ef7865e02e0855
Hacha_tq55@student.itstep.org <Hacha_tq55@student.itstep.org>
2024-10-08 20:19:33 +0300

    Добавьте файлы проекта.

---
+++ Homework_SysPr_9/Program.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.CodeAnalysis.CSharp.Scripting;
+using Microsoft.CodeAnalysis.Scripting;
+
+namespace Homework_SysPr_9
+{
+    class Program
+    {
+        static async Task Main()
+        {
+            while (true)
+            {
+                Console.WriteLine("Введите математическое выражение (или 'exit' для выхода): ");
+                string input = Console.ReadLine();
+
+                if (input == null)
+                    break;
+
+                try
+                {
+                    var result = await CalculateExpression(input);
+                    Console.WriteLine($"Результат:  {result}");
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"Ошибка: {ex.Message}");
+                }
+            }
+        }
+
+        static async Task<double> CalculateExpression(string expression)
+        {
+            var options = ScriptOptions.Default
+                .AddReferences(typeof(Math).Assembly)
+                .AddImports("System", "System.Math");
+
+            return await CSharpScript.EvaluateAsync<double>(expression, options); 
+        }
+    }
+}
